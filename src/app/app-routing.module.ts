6ae6ea62082144b2b82b549224896e1e8f23d227
ahadsenja <hamdanilm96@gmail.com>
2021-10-30 02:06:34 +0800

    reupload all files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { FeatureComponent } from './feature/feature.component';
+import { BasicFormComponent } from './form/basic-form/basic-form.component';
+import { ReactiveFormComponent } from './form/reactive-form/reactive-form.component';
 
 const routes: Routes = [
   { path: '', pathMatch:'full', redirectTo:'/home' },
   { path: 'home', component: HomeComponent },
   { path: 'feature', component: FeatureComponent },
+  { path: 'basic-form', component: BasicFormComponent },
+  { path: 'reactive-form', component: ReactiveFormComponent },
 ];
 
 @NgModule({
